29f6f5761e4c9f39de480066c983a36d34d16a65
priyanka944-hub <ypriyapriyanka1@gmail.com>
2023-07-26 11:01:23 +0530

    first

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-document.getElementById("root").innerText = "hello"
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "iam h2"),
+  ]);
+  
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "iam h2"),
+  ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+  
